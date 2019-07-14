d0e6894b3d6fdb39e3519a1c0da41df45f4d74da
purplejiang <1432819412@qq.com>
2019-07-14 22:38:16 +0800

    hook

---
+++ src/components/hook-test/event.js
@@ -0,0 +1,25 @@
+import React, { useState, useCallback } from 'react';
+import { useEventListener } from '../../hook/useEventListener';
+
+const Event = () => {
+  const [location, setLocation] = useState({ x: 0, y: 0 });
+
+  // 利用useCallback来处理回调
+  // ... 这里依赖将不会发生改变
+  const handler = useCallback(
+    ({ clientX, clientY }) => {
+      setLocation({ x: clientX, y: clientY });
+    },
+    [setLocation]
+  );
+
+  useEventListener('mousemove', handler);
+  return (
+    <div>
+      event location:
+      {location.x},{location.y}
+    </div>
+  );
+};
+
+export default Event;
